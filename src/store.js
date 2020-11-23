35f1e38a15c0fb8a86a84e16bd3dca53a76f518c
Abhishekdawer <abhishekdawer045@gmail.com>
2020-11-23 16:45:42 +0530

    frontend-amazona

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import {createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
+
+const initialState= {};
+const reducer = combineReducers({
+    productList: productListReducer,
+})
+const composeEnhancer = window.__REDU_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
+export default store;
